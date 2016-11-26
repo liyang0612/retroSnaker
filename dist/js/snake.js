d4fc72447083111567f0bc3e8a60267bdaaa7fe0
356514033 <liyang0612@github.com>
2016-11-26 12:42:00 +0800

    环境搭建

---
+++ dist/js/snake.js
@@ -0,0 +1,4 @@
+/**
+ * Created by 李洋 on 2016/11/26.
+ */
+console.log(2)
---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+//贪吃蛇开发环境配置
+var gulp = require('gulp'),
+    mincss = require('gulp-minify-css'),
+    minjs=require('gulp-jshint'),
+    browserSync=require('browser-sync').create();
+
+gulp.task('css',function () {
+    gulp.src('src/css/*')
+        .pipe(mincss())
+        .pipe(gulp.dest('dist/css'))
+        .pipe(browserSync.stream());
+})
+
+gulp.task('js',function () {
+    gulp.src('src/js/*')
+        .pipe(minjs())
+        .pipe(gulp.dest('dist/js'))
+        .pipe(browserSync.stream());
+})
+
+gulp.task('server',function () {
+    browserSync.init({
+        'server':'dist'
+    });
+    gulp.watch('src/css/*',['css']);
+    gulp.watch('src/js/*',['js'])
+    gulp.watch('dist/*.html').on('change',browserSync.reload)
+})
+
+gulp.task('default',['server'])
