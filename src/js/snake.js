f3129a8833ce21aaf7c569fcdc2b80e3abc0240a
356514033 <liyang0612@github.com>
2016-12-03 14:10:42 +0800

    转弯完成

--- src/js/snake.js
+++ src/js/snake.js
@@ -1,12 +1,15 @@
 var entry = {
         snakeBox: document.getElementById('snake-box'),
+        snakeHead: [],
+        oldSnakeHead: '',
+        newSnakeHead: '',
         rows: 20,
         cols: 30,
         boxArr: []
     },
     randomS = randomSnake();//得到一个数组randomS[0]为蛇头，randomS[1]食物
 
-//初始化开始函数
+//初始化开始函数;
 function start() {
     for (var i = 0; i < entry.rows; i++) {
         entry.boxArr[i] = new Array();
@@ -16,10 +19,11 @@ function start() {
             //生成画布
             var div = document.createElement("div");
             if (entry.boxArr[i][j] == randomS[0]) {   //随机初始化蛇头位置
+                entry.snakeHead[0] = randomS[0]; //把蛇头的id保存下来
                 div.className = "snake";
-            }else if(entry.boxArr[i][j] == randomS[1]){  //随机初始化食物的位置
+            } else if (entry.boxArr[i][j] == randomS[1]) {  //随机初始化食物的位置
                 div.className = "food";
-            }else{
+            } else {
                 div.className = "block";
             }
 
@@ -30,17 +34,59 @@ function start() {
         }
     }
 }
+start();
+
+//判断转弯
+window.document.onkeydown = function (e) {
+    var e = e || window.event;
+    var keyValue = e.keyCode;
+    switch (keyValue) {
+        case 37:
+            entry.oldSnakeHead = entry.snakeHead[0];
+            entry.newSnakeHead = entry.snakeHead[0] -= 1;
+            move();
+            break;
+        case 38:
+            entry.oldSnakeHead = entry.snakeHead[0];
+            entry.newSnakeHead = entry.snakeHead[0] -= entry.cols;
+            move();
+            break;
+        case 39:
+            entry.oldSnakeHead = entry.snakeHead[0];
+            entry.newSnakeHead = entry.snakeHead[0] += 1;
+            move();
+            break;
+        case 40:
+            entry.oldSnakeHead = entry.snakeHead[0];
+            entry.newSnakeHead = entry.snakeHead[0] += entry.cols;
+            move();
+    }
+}
+
+//移动函数
+function move() {
+    var newSnkhead = entry.newSnakeHead;
+    var oldSnake = entry.snakeHead;
+    oldSnake.pop();
+    var newSnake = oldSnake;
+    newSnake.unshift(newSnkhead);
+    for (var i = 0; i < newSnake.length; i++) {
+        document.getElementById(newSnake[i]).className = "snake"
+    }
+    console.log(entry.oldSnakeHead, newSnkhead)
+    if (entry.oldSnakeHead != newSnkhead)
+        document.getElementById(entry.oldSnakeHead).className = "block"
+}
+
 //随机蛇头位置和食物的位置
 function randomSnake() {
-    var randomNum=[],i=0;
+    var randomNum = [], i = 0;
     //生成两个随机数添加到数组中
-    while (i<2){
+    while (i < 2) {
         var random = Math.floor(Math.random() * 20 * 30);
-        randomNum[i]=random;
+        randomNum[i] = random;
         i++
     }
     //返回这个数组作为蛇头和食物的初始位置
     return randomNum;
 }
-
-start();
